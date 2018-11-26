5183cb60d837286f25a04c85ca143512b4256f1c
Nancy Yao <nancy.yao2@gmail.com>
2018-11-26 09:38:05 -0600

    ran deploy

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cdd453c2c7d7a3d33aedcd85d96e7577.js"
+  "/precache-manifest.d218c249c4fb8709a5519295a27fe578.js"
 );
 
 workbox.clientsClaim();
