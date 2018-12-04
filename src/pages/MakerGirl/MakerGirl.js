7c7c25d32a630bb8516c077a031fc681ac2ffefe
Nancy Yao <nancy.yao2@gmail.com>
2018-12-04 15:06:28 -0600

    MakerGirl page

--- src/pages/MakerGirl/MakerGirl.js
+++ src/pages/MakerGirl/MakerGirl.js
@@ -53,8 +53,8 @@ class MakerGirl extends Component {
                </Col>
                <Col xs={5} style={styles.col}>
                  <Caption side="caption-right" header="MakerGirl"
-                  text=<p>Founded in 2014 at University of Illinois Urbana Champaign, MakerGirl is a non-profit
-                  startup that aims to educated girls age 7-10 in STEM and foster passion and enthusiasm for
+                  text=<p>Founded in 2014 at University of Illinois Urbana Champaign, <a href="https://makergirl.us/">MakerGirl</a> is a non-profit
+                  startup that aims to educated girls ages 7-10 in STEM and foster passion and enthusiasm for
                   innovation and design.<br/><br/>At each themed session, girls learn about 3D printing and then
                   design and create their own 3D object to take home.</p>/>
                </Col>
@@ -65,7 +65,7 @@ class MakerGirl extends Component {
                 <Caption side="caption-left" header="Starting a chapter"
                  text=<p>In January 2018, I connected with Elizabeth Engele, co-founder of MakerGirl, to discuss
                  expanding their reach to the Evanston/Chicago area.<br/><br/>With the help of my co-founders,
-                 Rika Ko and Amanda Mirande, we began meeting with the UIUC team and organizing our own <b>Northwestern
+                 <a href="http://www.rika-ko.com/"> Rika Ko</a> and Amanda Mirande, we began meeting with the UIUC team and organizing our own <b>Northwestern
                  Academy</b>.<br/><br/>The picture to the right is from a night in Chicago spent with the UIUC team
                  members.</p>/>
               </Col>
@@ -79,18 +79,29 @@ class MakerGirl extends Component {
                  <img className="detail-image-left" src={MGSession1}/>
                </Col>
                <Col xs={5} style={styles.col}>
-                 <Caption side="caption-right" header="First sessions"
-                  text=<p>We held our first two sessions in Spring 2018, reaching local girls and providing a fun
-                  environment for them to learn about 3D printing and technology.</p>/>
+                 <Caption side="caption-right" header="Accomplishments"
+                  text=<p>&bull; Built a network from scratch of Evanston and surrounding
+                  Chicago area elementary schools, public libraries, and parent associations<br/><br/>
+                  &bull; Organized four successful sessions in 2018<br/><br/>
+                  &bull; Laid the foundation of the Northwestern Academy, establishing session
+                  logistics and building a team of 12 female university students<br/><br/>
+                  &bull; Partnered with established organizations, such as the Girl Scouts of Greater
+                  Chicago and Northwest Indiana and Codeverse, and businesses such as Fast Radius
+                  </p>/>
                </Col>
             </Row>
 
             <Row className="detail-row">
               <Col xs={5} style={styles.col}>
-                <Caption side="caption-left" header="Moving forward..."
-                 text=<p>MakerGirl and the NU academy in particular continue to grow as we partner with different
-                 groups such as CodeVerse and Girl Scouts, and enlist new team members. We now have a team of 16
-                 at NU, and hold bimonthly sessions of up to 10 girls each.</p>/>
+                <Caption side="caption-left" header="Teaching the next generation"
+                 text=<p>Each bimonthly session brings in up to 12 girls. We teach design thinking
+                 by allowing the girls to brainstorm their own designs for 3D prints.
+                 Mentors assist the girls with implementing these designs in TinkerCAD, a kid-friendly
+                 solid modeling app. The girls get to watch their ideas come to life in the 3D printers
+                 and take them home.<br/><br/>
+                 MakerGirl and the NU academy are growing faster than ever, and I am so proud
+                 to be a part in this organization. As we increase the numbers of
+                 our team and our connections with the local community, I see only great things in our future.</p>/>
               </Col>
                <Col xs={7} style={styles.col}>
                  <img className="detail-image-right" src={MGSession2}/>
--- src/sections/Developer/Developer.js
+++ src/sections/Developer/Developer.js
@@ -18,13 +18,49 @@ class Developer extends Component {
           <p>i'm a developer...</p>
         </header>
         <div className="Dev-body">
-          <Link to="/dev-instagram"><img src={instagram} className="Dev-mockup-phone" alt="instagram" /></Link>
-          <Link to="/dev-insync"><img src={insync} className="Dev-mockup-phone" alt="insync" /></Link>
-          <Link to="/dev-strugglebus"><img src={strugglebus} className="Dev-mockup-phone" alt="strugglebus" /></Link>
+          <Link to="/dev-instagram">
+            <div class="dev-overlay-container">
+              <img src={instagram} className="Dev-mockup-phone" alt="instagram" />
+              <div class="dev-overlay-phone">
+                <p class="dev-overlay-text">Instagram</p>
+              </div>
+            </div>
+          </Link>
+          <Link to="/dev-insync">
+            <div class="dev-overlay-container">
+              <img src={insync} className="Dev-mockup-phone" alt="insync" />
+              <div class="dev-overlay-phone">
+                <p class="dev-overlay-text">InSync</p>
+              </div>
+            </div>
+          </Link>
+          <Link to="/dev-strugglebus">
+            <div class="dev-overlay-container">
+              <img src={strugglebus} className="Dev-mockup-phone" alt="strugglebus" />
+              <div class="dev-overlay-phone">
+                <p class="dev-overlay-text">StruggleBus</p>
+              </div>
+            </div>
+          </Link>
         </div>
+
         <div className="Dev-body">
-          <Link to="/dev-buildher"><img src={buildher} className="Dev-mockup-desktop" alt="buildher" /></Link>
-          <Link to="/dev-collablab"><img src={collablab} className="Dev-mockup-desktop" alt="collablab" /></Link>
+          <Link to="/dev-buildher">
+            <div class="dev-overlay-container">
+              <img src={buildher} className="Dev-mockup-desktop" alt="buildher" />
+              <div class="dev-overlay-desktop">
+                <p class="dev-overlay-text">BuildHer</p>
+              </div>
+            </div>
+          </Link>
+          <Link to="/dev-collablab">
+            <div class="dev-overlay-container">
+              <img src={collablab} className="Dev-mockup-desktop" alt="collablab" />
+              <div class="dev-overlay-desktop">
+                <p class="dev-overlay-text">CollabLab</p>
+              </div>
+            </div>
+          </Link>
         </div>
       </div>
     );
